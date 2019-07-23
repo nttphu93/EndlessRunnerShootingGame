22264be13c40b2f865ab9777fdbde4a7c50560c3
nttphu93 <nttphu93@gmail.com>
2019-07-23 20:06:41 +0700

    implement Jump

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -6,9 +6,11 @@ public class PlayerControl : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float timeToWaitForDodging = 0.2f;
+    [SerializeField] float jumpPower = 5f;
     //Cached References
     Rigidbody myRigidbody;
     bool isButtonReady = true;
+    bool isJumpReady = true;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
@@ -17,16 +19,22 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isButtonReady)
-        {
-            StartCoroutine(TranslateLeftRight(Input.GetAxisRaw("Horizontal")));
-        }
-        
+       
     }
     //called in fixedUpdate because we work with physic
     void FixedUpdate()
     {
+        Debug.Log(myRigidbody.velocity.y);
         AutoForward();
+        if (isButtonReady)
+        {
+            StartCoroutine(TranslateLeftRight(Input.GetAxisRaw("Horizontal")));
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && isJumpReady)
+        {
+            Jump();
+        }
+        if(myRigidbody.velocity.y == 0) { isJumpReady = true; }
     }
     IEnumerator TranslateLeftRight(float directon)
     {
@@ -34,6 +42,11 @@ public class PlayerControl : MonoBehaviour
         isButtonReady = false;
         yield return new WaitForSeconds(timeToWaitForDodging);
         isButtonReady = true;
+    }
+    void Jump()
+    {
+        isJumpReady = false;
+        myRigidbody.velocity = Vector3.up * jumpPower * Time.deltaTime;
     }
     void AutoForward()
     {
